97dc09c48b7f8744bae73b9bb92e74010ed6bc38
sawaken-tech <sawapaca@gmail.com>
2020-06-06 18:02:50 +0900

    user_create

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+  
+  def after_update_path_for(resource)
+    root_path
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
   # end
+
 
   # POST /resource
   # def create
