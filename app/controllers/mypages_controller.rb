2e3d79023ba6bcc3d46b2b9e257732a43fc9fe05
sawaken-tech <sawapaca@gmail.com>
2020-07-23 02:51:15 +0900

    mypage
    マイページ実装途中

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,14 +1,19 @@
 class MypagesController < ApplicationController
 
-  # before_action :set_user
+  before_action :set_user, :set_question
 
   def show
   end
 
-  # private
+  private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+    @answer = Answer.find(params[:id])
+  end
 
 end
