08156b50ff4a0f02f77db23207a50b3ac0a8fec0
sawaken-tech <sawapaca@gmail.com>
2020-08-06 08:24:42 +0900

    fix

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,9 +1,9 @@
 class RatesController < ApplicationController
 
   def create
-    @rate = Rate.new(user_id: @current_user_id: params[:question_id])
+    @rate = Rate.new(user_id: @current_user_id, question_id: params[:question_id])
     @rate.save
-    redirect_to("/questions/#{params[:question_id]")
+    redirect_to("/questions/#{params[:question_id]}")
   end
 
 end
