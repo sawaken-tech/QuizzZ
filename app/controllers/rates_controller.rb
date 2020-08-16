b919411cb5f6e4962b8b50e92470690ec8d12701
sawaken-tech <sawapaca@gmail.com>
2020-08-16 14:20:33 +0900

    iine_update

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -2,12 +2,24 @@ class RatesController < ApplicationController
 
   def create
     @question = Qusetion.find(params[:question_id])
-    @question.iine(current_user)
+    unless @question.iine?(current_user)
+      @question.iine(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 
   def destroy
     @question = Rate.find(params[:id]).question
-    @question.uniine(current_user)
+    if @question.iine?(current_user) 
+      @question.uniine(current_user)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+    end
   end
 
 end
