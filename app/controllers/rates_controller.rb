7fc2fe04a18106500394f471904d0cd8cdc0b2af
sawaken-tech <sawapaca@gmail.com>
2020-08-20 18:38:02 +0900

    Redo

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,25 +1,11 @@
 class RatesController < ApplicationController
 
   def create
-    @question = Qusetion.find(params[:question_id])
-    unless @question.iine?(current_user)
-      @question.iine(current_user)
-      respond_to do |format|
-        format.html { redirect_to request.referrer || root_url }
-        format.js
-      end
-    end
+
   end
 
   def destroy
-    @question = Rate.find(params[:id]).question
-    if @question.iine?(current_user) 
-      @question.uniine(current_user)
-      respond_to do |format|
-        format.html { redirect_to request.referrer || root_url }
-        format.js
-      end
-    end
+
   end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,19 +4,5 @@ class Question < ApplicationRecord
   has_many   :comments
   has_one    :answer, dependent: :destroy
   validates :title, :sentence, :question_format, presence: true
-  has_many :iine_users, through: :rates, source: :user
-
-  def iine(user)
-    rates.create(user_id: user.id)
-  end
-
-  def uniine(user)
-    rates.find_by(user_id: user.id).destory
-  end
-
-  def iine?(user)
-    iine_users.include?(user)
-  end
-
 
 end
