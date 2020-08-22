1bf24dc67c318bbc0b440084a1484414289ff976
sawaken-tech <sawapaca@gmail.com>
2020-08-22 23:25:45 +0900

    create

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,7 +1,9 @@
 class RatesController < ApplicationController
 
   def create
-
+    @rate = Rate.new(user_id: @current_user.id, question_id: params[:question_id])
+    @rate.save
+    redirect_to("/questions/#{params[question_id]}")
   end
 
   def destroy
