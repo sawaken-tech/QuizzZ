bdda21e9704ac4ede6046e3a695cbde3443ec128
sawaken-tech <sawapaca@gmail.com>
2020-09-20 07:33:34 +0900

    development_test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            'メールアドレス',
-    password:             'パスワード',
+    user_name:            ENV['メールアドレス'],
+    password:             ENV['パスワード']
     authentication:       'plain',
     enable_starttls_auto:  true
   }
