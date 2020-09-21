9e037bff30875cdcf1f0b9b2cff77867bd1ffda0
sawaken-tech <sawapaca@gmail.com>
2020-09-21 13:05:17 +0900

    env_add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            ENV['メールアドレス'],
-    password:             ENV['パスワード']
+    user_name:            ENV['MAIL_SECRET_USER_NAME'],
+    password:             ENV['MAIL_SECRET_PASSWORD']
     authentication:       'plain',
     enable_starttls_auto:  true
   }
