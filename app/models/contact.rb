d56ec37c0c40cabf41d3586f0c8bae04611e50c5
sawaken-tech <sawapaca@gmail.com>
2020-09-23 08:25:17 +0900

    contact_form_error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,8 @@ class Contact include ActiveModel::Model
 
   attr_accessor :name, :email, :message
  
-  validates :name, :presence => {:message => '名前を入力してください'}
-  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
+  validates :name, presence: {message: '名前を入力してください'}
+  validates :email, presence: {message: 'メールアドレスを入力してください'}
+  validates :message, presence: {message: 'お問い合わせ内容を入力してください'}
 
 end
