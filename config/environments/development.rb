c791de501becd6d3690fa927ae86c9303a8e849f
sawaken-tech <sawapaca@gmail.com>
2020-09-22 20:19:02 +0900

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            ENV['MAIL_SECRET_USER_NAME'],
-    password:             ENV['MAIL_SECRET_PASSWORD']
+    password:             ENV['MAIL_SECRET_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
