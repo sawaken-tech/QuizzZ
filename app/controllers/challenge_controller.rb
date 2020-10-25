4c9260d94d3e19dc8e37101e36825d61c0f7b57b
sawaken-tech <sawapaca@gmail.com>
2020-10-26 07:45:58 +0900

    randam__test

--- app/controllers/challenge_controller.rb
+++ app/controllers/challenge_controller.rb
@@ -2,7 +2,7 @@ class ChallengeController < ApplicationController
 
   # 試験モード実装予定
   def test
+    @rand = Question.where( 'id >= ?', rand(Question.first.id..Question.last.id) ).first
   end
-
 
 end
