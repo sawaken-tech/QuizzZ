739138159bb9603afe3c3bed46d5b9d4ffab0301
sawaken-tech <sawapaca@gmail.com>
2020-11-21 13:15:18 +0900

    js__add__id

--- app/assets/javascripts/challenge.js
+++ app/assets/javascripts/challenge.js
@@ -2,3 +2,12 @@
 // 問題の回答は一度
 // 問題を回答したら次の問題へ
 // 正解数を集計する
+
+
+$(function(){
+// 各問題「box+数字」IDを付与する
+  $('.playPage__test__container').each(function(index, element) {
+    $(element).attr('id','box' + (index + 1).toString().padStart(2, '0'));
+    });
+
+});
