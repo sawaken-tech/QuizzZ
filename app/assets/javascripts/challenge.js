f8929304c36a205471c72d2f0115e070c2ce1b04
sawaken-tech <sawapaca@gmail.com>
2020-11-29 12:24:34 +0900

    js_nextAnswer

--- app/assets/javascripts/challenge.js
+++ app/assets/javascripts/challenge.js
@@ -8,13 +8,19 @@ $(function(){
 // 各問題「box+数字」IDを付与する
   $('.playPage__test__container').each(function(index, element) {
     $(element).attr('id','box' + (index + 1).toString().padStart(2, '0'));
+    $(element).addClass('close');
+    $('#box01').removeClass('close');
     });
 
 // NEXTボタンを押すと２問目の問題にする
-  function nextAnser() {
-    $("#nextbtn").on("click", function(){
-      //jsでCSSを操作するところから
-    })
+  function nextAnswer() {
+    $(".playPage__test__btnBox__btn").on("click", function(){
+      $('#box01').removeClass('open');
+      $('#box01').addClass('close');
+      $('#box02').removeClass('close');
+      $('#box02').addClass('open');
+    });
   }
+  nextAnswer();
 
 });
